ffba0e86f0e6772cf11f415d59ac36f650612fe8
Maxim Bogdanov <bogdanov.mv.95@gmail.com>
2020-12-18 03:06:45 +0600

    make it better

--- config.go
+++ config.go
@@ -2,5 +2,5 @@ package main
 
 const (
 	token   = "1488492813:AAFWcpzyTh4aOeP5anW--ShkxHD3NaJLhMI"
-	webhook = "https://telebot-duremika.herokuapp.com"
+	webhook = "https://go-telebot-duremika.herokuapp.com/"
 )
